6e231fad78fc21ad190a0544c6535f624d527ed6
Benjamin Catouillard <catouillard.benjamin@gmail.com>
2020-05-27 14:29:50 +0200

    feat(S03-01): Recycler View

--- S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
+++ S03.01-Exercise-RecyclerView/app/src/main/java/com/example/android/sunshine/ForecastAdapter.java
@@ -1,7 +1,9 @@
 package com.example.android.sunshine;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
@@ -12,23 +14,46 @@ public class ForecastAdapter extends RecyclerView.Adapter<ForecastAdapter.Foreca
 
     private String[] mWeatherData;
 
-    ForecastAdapter(){
+    public ForecastAdapter(){
 
     }
 
-    class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
+    public class ForecastAdapterViewHolder extends RecyclerView.ViewHolder {
 
         public final TextView mWeatherTextView;
 
-        ForecastAdapterViewHolder(View view){
+        public ForecastAdapterViewHolder(View view) {
             super(view);
             mWeatherTextView = (TextView) view.findViewById(R.id.recyclerview_forecast);
         }
+    }
 
         @Override
         public ForecastAdapterViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
-            return null;
+            Context context = viewGroup.getContext();
+            int layoutIdForListItem = R.layout.forecast_list_item;
+            LayoutInflater inflater = LayoutInflater.from(context);
+            boolean shouldAttachToParentImmediately = false;
+
+            View view = inflater.inflate(layoutIdForListItem, viewGroup, shouldAttachToParentImmediately);
+            return new ForecastAdapterViewHolder(view);
         }
-    }
+
+        @Override
+        public void onBindViewHolder(ForecastAdapterViewHolder forecastAdapterViewHolder, int position) {
+            String weatherForThisDay = mWeatherData[position];
+            forecastAdapterViewHolder.mWeatherTextView.setText(weatherForThisDay);
+        }
+
+        @Override
+        public int getItemCount() {
+            if (null == mWeatherData) return 0;
+            return mWeatherData.length;
+        }
+
+        public void setWeatherData(String[] weatherData) {
+            mWeatherData = weatherData;
+            notifyDataSetChanged();
+        }
 }
 
